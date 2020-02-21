b10891bcdc8ace4fa75b3765ebe2870adf7a9651
Anton Plisnier <anttongp@gmail.com>
2020-02-21 17:33:00 +0000

    Step 13; can check if dock is full or not; laid groundwork to check if bikes are visible (.view), still needs implementation; test pass (just)

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,41 @@
 require_relative "bike"
 
 class DockingStation
+
+	attr_reader = :dock
+
+
+	def initialize
+		@@dock = {
+			:pos1 => "Empty"
+		}
+	end
+
+
+	def dock_bike
+
+		@@dock.each { |position, value| 
+
+			if value == "Empty" 
+				@@dock[position] = "Occupied"
+				return "Thank you, your bike has been stored in position [#{position.to_s.chars[-1].to_i}]" 
+			else
+				return "Dock is full!"
+			end
+		}
+
+
+	end
+
+
+	def view 
+		false
+	end
+
+
 	def release_bike
 		Bike.new
+		raise "NO NO NO NO NO" unless view == true
 	end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,38 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-	it {is_expected.to respond_to(:release_bike)}
-	it { expect(subject.release_bike).to be_working }
+	# it {is_expected.to respond_to(:release_bike)}
+	# it { expect(subject.release_bike).to be_working }
+
+	context ".dock_bike" do
+	
+		it "will dock a given bike if there is space" do
+			expect(subject.dock_bike).to eq "Thank you, your bike has been stored in position [1]"
+		end
+	
+		it "will reject me if there is no space" do
+			expect{subject.dock_bike}.not_to change{:@@dock}#.from("Empty").to("Occupied")
+		end
+
+	end
+	context ".view" do
+		it "will show if bike exists" do
+			expect(subject.view).to eq false
+		end
+	end
+
+	context ".release_bike, bike availability" do
+		it "no bikes" do
+			expect{ subject.release_bike }.to raise_error("NO NO NO NO NO")		
+		end
+		# it 'dock is full' do
+		# end
+		# it 'dock multiple bikes' do
+		# end
+ 	end
 end
+
+
+
+# class_variable_get
+# assigns()
