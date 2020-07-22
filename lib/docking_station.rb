0ad00d8291787adac8e26e91a68cf9cc535e21f7
Anton Plisnier <anttongp@gmail.com>
2020-07-22 16:48:53 +0100

    dock now uses procs with a single action method. bug fixes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,40 +2,48 @@ require_relative "bike"
 
 class DockingStation
 
-	attr_reader = :dock
-
-
 	def initialize
 		@@dock = {
-			:pos1 => "Empty"
+			:pos1 => "Empty",
+			:pos2 => "Empty",
+			:pos3 => "Empty",
+			:pos4 => "Empty",
+			:pos5 => "Empty",
+			:pos6 => "Empty",
+			:pos7 => "Empty",
+			:pos8 => "Empty",
+			:pos9 => "Empty",
+			:pos0 => "Empty"
 		}
+		@release = lambda { |p| @@dock[p] = "Empty"; puts "A bike has been released from position #{p.to_s.chars[-1].to_i}"}
+		@dock_to = lambda { |p| @@dock[p] = "Occupied"; puts "The bike was stored in position #{p.to_s.chars[-1].to_i}"}
 	end
 
-
-	def dock_bike
-
-		@@dock.each { |position, value| 
-
-			if value == "Empty" 
-				@@dock[position] = "Occupied"
-				return "Thank you, your bike has been stored in position [#{position.to_s.chars[-1].to_i}]" 
-			else
-				return "Dock is full!"
-			end
-		}
-
-
+	def full?
+		@@dock.values.count("Occupied") == 10 ? true : false
 	end
 
+	def action(switch)
+		case switch
+		when "dock"
+			@@dock.each { |position,value|
+				if value == "Empty"
+					@dock_to.call(position)
+					break
+				elsif full?() == true
+					puts "Dock is full!"
+					break
+				end
+			}
 
-	def view 
-		false
+		when "release"				
+			@@dock.each { |position,value|
+				if value == "Occupied"
+					@release.call(position)
+					break
+				end
+			}	
+
+		end 
 	end
-
-
-	def release_bike
-		Bike.new
-		raise "NO NO NO NO NO" unless view == true
-	end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,38 +1,34 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-	# it {is_expected.to respond_to(:release_bike)}
-	# it { expect(subject.release_bike).to be_working }
 
-	context ".dock_bike" do
-	
+	context "action" do
 		it "will dock a given bike if there is space" do
-			expect(subject.dock_bike).to eq "Thank you, your bike has been stored in position [1]"
+			# expect(subject.dock.count("Occupied")).to eq(1)
+			expect(subject.action("dock")).to eq("A bike has been released from position 1")
 		end
-	
 		it "will reject me if there is no space" do
-			expect{subject.dock_bike}.not_to change{:@@dock}#.from("Empty").to("Occupied")
+			expect{subject.dock_bike}.not_to change{:@@dock}
 		end
-
 	end
+
 	context ".view" do
-		it "will show if bike exists" do
+		it "will return true if dock is full" do
+			docking_station = DockingStation.new
+
+			#expect(subject.release_bike).to eq("NO NO NO NO NO")
+			expect(subject.view).to eq true
+		end
+		it "will return false if dock has availability" do
 			expect(subject.view).to eq false
 		end
 	end
 
-	context ".release_bike, bike availability" do
+	context ".release_bike" do
 		it "no bikes" do
 			expect{ subject.release_bike }.to raise_error("NO NO NO NO NO")		
 		end
-		# it 'dock is full' do
-		# end
-		# it 'dock multiple bikes' do
-		# end
  	end
+
 end
 
-
-
-# class_variable_get
-# assigns()
